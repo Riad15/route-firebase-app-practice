514bd1605b437ba6cc3c1a0afeb209d70f0baef4
asif riad <asifriad15@gmail.com>
2022-04-10 02:48:41 +0600

    react route and firebase instal and home section added

---
+++ src/component/Login/Login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div>
+            <h2>log in sectin</h2>
+        </div>
+    );
+};
+
+export default Login;
---
+++ src/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBr3O3H295DSC3HGY9HCP0LEg_r7kPPh5Q",
+    authDomain: "router-firebase-app-a152e.firebaseapp.com",
+    projectId: "router-firebase-app-a152e",
+    storageBucket: "router-firebase-app-a152e.appspot.com",
+    messagingSenderId: "697799877043",
+    appId: "1:697799877043:web:72a55a68c5e637b005abc6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
